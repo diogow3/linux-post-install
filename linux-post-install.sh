e92a54a1bea714cffd94ba97b15cdda3a013e98b
Diogo Oliveira dos Santos <diogow3@gmail.com>
2023-05-21 14:16:14 -0300

    Update linux-post-install.sh

--- linux-post-install.sh
+++ linux-post-install.sh
@@ -152,12 +152,11 @@ then
 	sudo usermod -aG docker $USER
 
 	# github cli
-	type -p curl >/dev/null || sudo apt install curl -y
-	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-	&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+	#type -p curl >/dev/null || sudo apt install curl -y
+	#curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+	#&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+	#&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+	sudo apt update; sudo apt install -y gh
 
 	# java 17 jdk eclipse temurin
 	sudo apt install -y wget apt-transport-https
@@ -177,7 +176,7 @@ then
 
 	# python pipenv
 	sudo apt install -y python3 python3-pip python3-venv
-	#sudo apt install -y pipenv
+	#sudo apt install -y pipenv # broken until ubuntu 23.04
 	pip install --user pipenv
 	echo '# python .venv in project folder' >> ~/.profile
 	echo 'export PIPENV_VENV_IN_PROJECT=true' >> ~/.profile
@@ -302,6 +301,7 @@ then
 
 	# After rebooting, open extensions manager and install
 	# Dash to Dock, AppIndicator, Desktop Icons, Wireless hid
+	# Open Dash to Dock settings, import dashtodock_settings from the 'extensions' folder
 
 fi # end Fedora
 
