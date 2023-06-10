fda6f859c2b85ea0e1f403ccd57a46da4d44d8e4
Diogo Oliveira dos Santos <diogow3@gmail.com>
2023-06-10 19:31:36 -0300

    update

--- linux-post-install.sh
+++ linux-post-install.sh
@@ -155,7 +155,21 @@ then
 	sudo apt update; sudo apt install -y code
 
 	# dotnet
+	sudo touch /etc/apt/preferences
+	APT_SRC="$(apt-cache policy '~ndotnet.*' | grep -v microsoft | grep -v security | grep '/ubuntu' | cut -d"/" -f3 | sort -u)"
+
+	echo "
+	Package: dotnet* aspnet* netstandard*
+	Pin: origin \"${APT_SRC}\"
+	Pin-Priority: -10" | sudo tee -a /etc/apt/preferences > /dev/null
+
+	declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+	wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+	sudo dpkg -i packages-microsoft-prod.deb
+	rm packages-microsoft-prod.deb
+
 	sudo apt update; sudo apt install -y dotnet-sdk-6.0
+
 
 	# homebrew
 	(echo; echo '# Set PATH, MANPATH, etc., for Homebrew.') >> ~/.profile
