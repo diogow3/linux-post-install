efec10f78e4e47373ff8efdf147c48b76206afd7
Diogo Oliveira dos Santos <diogow3@gmail.com>
2025-05-07 17:31:00 -0300

    Update nvm

--- linux-post-install.sh
+++ linux-post-install.sh
@@ -474,7 +474,7 @@ sdk install maven
 sdk install gradle
 
 # nodejs lts - via nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 source "$HOME/.nvm/nvm.sh"
 nvm install --lts
 
